6fdcc9aeb9ded665d447ac1d46336b842d9fc816
Aleksei <eastonsc6@yandex.ru>
2020-08-11 10:34:03 +0300

    алерты для проверки

--- src/js/index.js
+++ src/js/index.js
@@ -16,5 +16,8 @@ $(function () {
         prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
     });
+    $(document).ready(function () {
+        alert("index.js");
+    });
 
 });
